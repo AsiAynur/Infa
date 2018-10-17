edb3e098d2985e2e4fe23ea90a081226a3334199
Aynur <aynuradem55@gmail.com>
2018-10-17 23:49:49 +0300

    сделала 4 задание

---
+++ Task4/inform4.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main (){
+int a, b, result;
+cin >> a >> b;
+	if (b==0) {
+		cout << "impossible" << endl;
+		}
+	else {
+		result = a / b;
+		cout << result << endl;
+	}
+	system ("pause");
+}
+
